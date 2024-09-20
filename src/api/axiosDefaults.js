93db6683f72fd7f917ce434e9288af1f45fad94b
Malin Nilsson <malin_christina@yahoo.se>
2024-09-20 16:19:26 +0000

    add sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://whispering-plateau-35247-5ab0a29abf01.herokuapp.com/"
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
