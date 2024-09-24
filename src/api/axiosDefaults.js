1bd9dd115f262a7cf2ca03f9008f9a2f97b900bd
Malin Nilsson <malin_christina@yahoo.se>
2024-09-24 17:37:31 +0000

    access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://whispering-plateau-35247-5ab0a29abf01.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
