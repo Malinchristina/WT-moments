aa97764cef82181a6f264418addcba3a51c8d583
Malin Nilsson <malin_christina@yahoo.se>
2024-09-27 14:33:15 +0000

    create form for user

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://whispering-plateau-35247-5ab0a29abf01.herokuapp.com/"
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = "https://whispering-plateau-35247-5ab0a29abf01.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
