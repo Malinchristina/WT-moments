5c013e54fe20efc7e3d945ad39b543035e77c687
Malin Nilsson <malin_christina@yahoo.se>
2024-10-03 10:51:06 +0000

    set up postpage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,7 +7,6 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import Post from "./Post";
 
 function PostPage() {
   const { id } = useParams();
@@ -33,7 +32,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <p>Post component</p>
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
